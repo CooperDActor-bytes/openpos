2a806b445ed07b80c886a44ce30436a6e8194a5b
CooperDActor-bytes <151992386+CooperDActor-bytes@users.noreply.github.com>
2024-02-20 15:36:41 +1100

    Add files via upload

---
+++ Netlogon/old Netlogon.bat
@@ -0,0 +1,24 @@
+rem ***********************
+rem * global login script *
+rem ***********************
+
+if %COMPUTERNAME% == server GOTO NO_EDRIVE
+if %COMPUTERNAME% == SERVER GOTO NO_EDRIVE
+
+rem set up drives
+rem **************
+net use e: /d
+net use e: \\ws1\AussiePizzaCo
+net use z: /d
+Net use z: \\ws1\screensaver
+net time \\ws1 /SET /YES
+
+:No_EDRIVE
+rem Set up dos printing
+rem *******************
+net use lpt1: /d
+net use lpt2: /d
+net use lpt1: \\ws1\docket-1
+rem net use lpt2: \\ws3\report
+
+:End
---
+++ schtasks.bat
@@ -0,0 +1,2 @@
+cd\EBCOMS
+c:\ebcoms\schtasks /create /RU OWNER /RP OWNER /SC weekly /ST 01:00:00 /D Mon /TR C:\Ebcoms\winsales.exe /TN WinSalesRun
